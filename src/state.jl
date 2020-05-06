754de1db6c722d13fc5a5c56bfde652f9889a866
PvPisistratus <pvpisistratus@gmail.com>
2020-05-06 18:58:40 -0500

    Data files moved and prettified

--- src/state.jl
+++ src/state.jl
@@ -38,8 +38,8 @@ struct Pokemon
 end
 
 function Pokemon(i::Int64)
-    rankings = JSON.parsefile("data/rankings-1500.json")
-    gamemaster = JSON.parsefile("data/gamemaster.json")
+    rankings = JSON.parsefile("rankings-1500.json")
+    gamemaster = JSON.parsefile("gamemaster.json")
     cpm = CSV.read("CPMperLevel.csv", header = false)
     moves = parse.(Ref(Int64), split(rankings[i]["moveStr"], "-"))
     gmid = get_gamemaster_mon_id(rankings[i]["speciesId"], gamemaster)
