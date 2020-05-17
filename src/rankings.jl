c17eccbb615f76fa498e69960437b690b55293c6
PvPisistratus <pvpisistratus@gmail.com>
2020-05-17 18:20:27 -0500

    Removed inbounds again

--- src/rankings.jl
+++ src/rankings.jl
@@ -76,7 +76,7 @@ function run_empirical_teams(
     histogram = Hist(0.0:0.025:1.0)
     for i = 1:length(awayTeams)
         for j = 1:weights[i]
-            @inbounds fit!(
+            fit!(
                 histogram,
                 play_battle(State(homeTeam, awayTeams[i])),
             )
