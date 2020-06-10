29f71489b18b7cd7f710970f52cb027188d9e831
PvPisistratus <pvpisistratus@gmail.com>
2020-06-10 00:23:37 -0500

    Added imperfect information

--- src/strategy.jl
+++ src/strategy.jl
@@ -16,15 +16,53 @@ function DecisionMatrix()
     return DecisionMatrix(dmat)
 end
 
-function get_decision_matrix(state; battles_per_turn = 1000)
+function get_decision_matrix(
+    state;
+    battles_per_turn = 1000,
+    perfect_information = true,
+    active_mons::Array{Tuple{Int64, Int64}} = [],
+    meta::Array{Pokemon} = [],
+    weights::Array{Real} = []
+)
     d_matrix = DecisionMatrix()
     weights1 = get_possible_decisions(state)
     weights2 = get_possible_decisions(switch_agent(state))
-    if !iszero(sum(weights1)) && !iszero(sum(weights2))
-        for d1 in findall(isone, weights1), d2 in findall(isone, weights2)
-            next_state = play_turn(state, (d1, d2))
-            scores = get_battle_scores(next_state, battles_per_turn)
-            d_matrix.decision_matrix[d1, d2] = (mean(scores) - 3 * std(scores), mean(scores) + 3 * std(scores))
+    if perfect_information
+        if !iszero(sum(weights1)) && !iszero(sum(weights2))
+            for d1 in findall(isone, weights1), d2 in findall(isone, weights2)
+                next_state = play_turn(state, (d1, d2))
+                scores = get_battle_scores(next_state, battles_per_turn)
+                d_matrix.decision_matrix[d1, d2] = (mean(scores) - 3 * std(scores), mean(scores) + 3 * std(scores))
+            end
+        end
+    else
+        if !iszero(sum(weights1)) && !iszero(sum(weights2))
+            for d1 in findall(isone, weights1), d2 in findall(isone, weights2)
+                next_state = play_turn(state, (d1, d2))
+                scores1::Array{Float64} = []
+                scores2::Array{Float64} = []
+                for i = 1:battles_per_turn
+                    modified_state1 = next_state
+                    modified_state2 = next_state
+
+                    if !(2 in first.(active_mons))
+                        modified_state2 = @set modified_state2.teams[1].mons[2] = meta[rand(Categorical(weights ./ sum(weights)))]
+                    end
+                    if !(3 in first.(active_mons))
+                        modified_state2 = @set modified_state2.teams[1].mons[3] = meta[rand(Categorical(weights ./ sum(weights)))]
+                    end
+                    push!(scores2, play_battle(modified_state2))
+
+                    if !(2 in last.(active_mons))
+                        modified_state1 = @set modified_state1.teams[2].mons[2] = meta[rand(Categorical(weights ./ sum(weights)))]
+                    end
+                    if !(3 in last.(active_mons))
+                        modified_state1 = @set modified_state1.teams[2].mons[3] = meta[rand(Categorical(weights ./ sum(weights)))]
+                    end
+                    push!(scores1, play_battle(modified_state1))
+                end
+                d_matrix.decision_matrix[d1, d2] = (mean(scores1) - 3 * std(scores1), mean(scores2) + 3 * std(scores2))
+            end
         end
     end
     return d_matrix
@@ -63,11 +101,21 @@ end
 
 Strategy() = Strategy([], [], [], [])
 
-function Strategy(state::State; battles_per_turn::Int64 = 1000)
+function Strategy(
+    state::State;
+    battles_per_turn::Int64 = 1000,
+    perfect_information::Bool = true,
+    meta::Array{Pokemon} = [],
+    weights::Array{Real} = [],
+)
     strategy = Strategy()
     current_state = state
     while true
-        d_matrix = get_decision_matrix(current_state, battles_per_turn = battles_per_turn)
+        if perfect_information
+            d_matrix = get_decision_matrix(current_state, battles_per_turn = battles_per_turn)
+        else
+            d_matrix = get_decision_matrix(current_state, battles_per_turn = battles_per_turn, active_mons = strat.activeMons, meta = meta, weights = weights)
+        end
         is_empty(d_matrix) && return strategy
         decision = minimax(d_matrix)
         push!(strategy.decisions, decision)
