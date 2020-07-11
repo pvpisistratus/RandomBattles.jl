93e5eaca1392055d7fd38e4ea69ca1f638f6884e
PvPisistratus <pvpisistratus@gmail.com>
2020-07-11 12:37:32 -0500

    Bug fix with post charged move sender fast move cooldown

--- src/mechanics.jl
+++ src/mechanics.jl
@@ -165,7 +165,7 @@ function evaluate_charged_moves(state::BattleState)
             )
         end
         next_state = apply_buffs(next_state, cmp)
-        next_state = @set next_state.teams[cmp].mons[next_state.teams[cmp].active].fastMoveCooldown = 500
+        next_state = @set next_state.teams[cmp].mons[next_state.teams[cmp].active].fastMoveCooldown = 1000
         next_state = @set next_state.teams[get_other_agent(cmp)].mons[next_state.teams[get_other_agent(cmp)].active].fastMoveCooldown = -500
         next_state = @set next_state.chargedMovesPending[cmp] =
             ChargedAction(Move(0, 0.0, 0, 0, 0, 0.0, 0, 0, 0, 0), 0)
