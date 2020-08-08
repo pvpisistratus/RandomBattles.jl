35e2f0592a71036a96710f9b419afd90a9bd864f
PvPisistratus <pvpisistratus@gmail.com>
2020-08-08 18:37:24 -0500

    Added source of empirical data of PvPoke weights

--- src/pokemon_meta.jl
+++ src/pokemon_meta.jl
@@ -5,13 +5,32 @@ struct PokemonMeta
     weights::Distribution
 end
 
-function PokemonMeta(cup::String; data_key = "all")
-    resp = HTTP.get("https://silph.gg/api/cup/" * cup * "/stats/.json")
-    data = JSON.parse(String(resp.body))
-    silph_keys = collect(keys(data[data_key]))
-    mons = silph_to_pvpoke.(silph_keys)
-    meta_weights = map(x -> data[data_key][x]["percent"], silph_keys)
-    return PokemonMeta(Pokemon.(mons, cup = cup), Categorical(meta_weights ./ sum(meta_weights)))
+function PokemonMeta(cup::String; data_key = "all", source = "silph")
+    if source == "silph"
+        resp = HTTP.get("https://silph.gg/api/cup/" * cup * "/stats/.json")
+        data = JSON.parse(String(resp.body))
+        silph_keys = collect(keys(data[data_key]))
+        mons = silph_to_pvpoke.(silph_keys)
+        meta_weights = map(x -> data[data_key][x]["percent"], silph_keys)
+        return PokemonMeta(Pokemon.(mons, cup = cup), Categorical(meta_weights ./ sum(meta_weights)))
+    else if source == "pvpoke"
+        overrides = get_rankings("rankingoverrides")
+        cup_id = findfirst(x -> x["cup"] == cup, overrides)
+        mons = Pokemon.(map(x -> x["speciesId"], get_rankings(cup)), cup = cup)
+        weights = ones(length(mons))
+        for i = 1:length(overrides[cup_id]["pokemon"])
+            if haskey(overrides[cup_id]["pokemon"][i], "weight")
+                weight = overrides[cup_id]["pokemon"][i]["weight"]
+                speciesId = overrides[cup_id]["pokemon"][i]["speciesId"]
+                species_index = findfirst(x -> speciesId == x,
+                    map(x -> x["speciesId"], get_rankings(cup)))
+                if !isnothing(species_index)
+                    weights[species_index] = weight
+                end
+            end
+        end
+        PokemonMeta(mons, Categorical(weights ./ sum(weights)))
+    end
 end
 
 PokemonMeta() = PokemonMeta([], Categorical([1.0]))
