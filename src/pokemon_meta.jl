10b06a4730fb3dd600f7a483bcf68c2cc7239f33
PvPisistratus <pvpisistratus@gmail.com>
2020-08-10 13:00:41 -0500

    More print debugging

--- src/pokemon_meta.jl
+++ src/pokemon_meta.jl
@@ -20,6 +20,7 @@ function PokemonMeta(
         return PokemonMeta(Pokemon.(mons, cup = cup),
             Categorical(meta_weights ./ sum(meta_weights)))
     elseif source == "pvpoke"
+        println(league)
         overrides = get_rankings("rankingoverrides")
         cup_id = findfirst(
             x -> x["cup"] == cup && x["league"] == get_cp_limit(league),
