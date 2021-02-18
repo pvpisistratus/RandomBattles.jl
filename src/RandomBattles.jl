e8964c7bf3fb127e410cc113b0fd639d983a6439
itscoe <itslagle@coe.edu>
2021-02-18 09:39:10 -0600

    unincluding extraneous files: diff, learning, meta, rankings, strategy

--- src/RandomBattles.jl
+++ src/RandomBattles.jl
@@ -7,24 +7,24 @@ include("individual.jl")
 include("state.jl")
 include("individual_battle_state.jl")
 include("utility_functions.jl")
-include("diff.jl")
+#include("diff.jl")
 include("mechanics.jl")
 include("battle_logic.jl")
-include("pokemon_meta.jl")
-include("strategy.jl")
-include("rankings.jl")
-include("learning.jl")
+#include("pokemon_meta.jl")
+#include("strategy.jl")
+#include("rankings.jl")
+#include("learning.jl")
 
 export State, IndividualBattleState,
        Team, Individual,
        Pokemon,
-       Strategy, plot_strategy,
+       #Strategy, plot_strategy,
        get_possible_decisions,
        get_battle_score, get_battle_scores,
        gamemaster, greatRankings, ultraRankings, masterRankings,
-       play_turn, play_battle,
-       rank, PokemonMeta,
-       diff, vectorize,
-       crossover_and_mutate, play_nn_battle, play_nn_random_battle, testing_best
+       play_turn, play_battle
+       #rank, PokemonMeta,
+       #diff, vectorize,
+       #crossover_and_mutate, play_nn_battle, play_nn_random_battle, testing_best
 
 end # module
