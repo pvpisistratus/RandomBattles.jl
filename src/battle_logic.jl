975bc36f7e1f6da77c4ddaa52ef17649f61ac9b4
itscoe <itslagle@coe.edu>
2021-02-18 11:38:44 -0600

    Fixed bug in play_turn

--- src/battle_logic.jl
+++ src/battle_logic.jl
@@ -108,16 +108,12 @@ function play_turn(state::DynamicState, static_state::StaticState, decision::Tup
     dec = defaultDecision
     next_state = state
     if state.fastMovesPending[1] == Int8(0)
-        println("evaluating fast move")
         next_state = evaluate_fast_moves(state, static_state, 1)
-        RandomBattles.diff(state, next_state)
     end
     if state.fastMovesPending[2] == Int8(0)
-        println("evaluating fast move")
         next_state = evaluate_fast_moves(state, static_state, 2)
-        RandomBattles.diff(state, next_state)
     end
-    next_state, dec = queue_decision(state, static_state, dec, decision[1])
+    next_state, dec = queue_decision(next_state, static_state, dec, decision[1])
     next_state, dec = queue_decision((@set next_state.agent = Int8(2)), static_state, dec, decision[2])
     if dec.switchesPending[1].pokemon != Int8(0) || dec.switchesPending[2].pokemon != Int8(0)
         next_state = evaluate_switches(next_state, dec)
