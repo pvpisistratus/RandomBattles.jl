ef2c2d2e5ca361f5206a34883e8ee3cf908185a0
itscoe <itslagle@coe.edu>
2021-03-01 14:34:50 -0600

    Will I ever spell shielding correctly first try?

--- src/mechanics.jl
+++ src/mechanics.jl
@@ -174,7 +174,7 @@ function evaluate_charged_moves(state::DynamicState, static_state::StaticState, 
             Int8(100))) : state.teams[1].mons[3]),
             ], buffs_applied ? state.teams[1].buffs + static_state.teams[1].mons[3].chargedMoves[move_id].self_buffs : state.teams[1].buffs,
             max(Int8(0), state.teams[1].switchCooldown - Int8(20)), state.teams[1].shields, state.teams[1].active),
-            DynamicTeam((!sheilding ? state.teams[2].mons : @SVector[Int8(1) == state.teams[2].active ? DynamicPokemon(max(
+            DynamicTeam((!shielding ? state.teams[2].mons : @SVector[Int8(1) == state.teams[2].active ? DynamicPokemon(max(
                 Int16(0),
                 state.teams[2].mons[1].hp -
                 calculate_damage(
@@ -215,7 +215,7 @@ function evaluate_charged_moves(state::DynamicState, static_state::StaticState, 
             state.teams[2].active)], state.fastMovesPending)
     else
         DynamicState(@SVector[
-            DynamicTeam((!sheilding ? state.teams[1].mons : @SVector[Int8(1) == state.teams[1].active ? DynamicPokemon(max(
+            DynamicTeam((!shielding ? state.teams[1].mons : @SVector[Int8(1) == state.teams[1].active ? DynamicPokemon(max(
                 Int16(0),
                 state.teams[1].mons[1].hp -
                 calculate_damage(
