bea491bf1d69508ad4ff46160312895069c698c6
itscoe <itslagle@coe.edu>
2021-03-01 22:40:27 -0600

    Attempt to remove allocations from play_battle

--- src/battle_logic.jl
+++ src/battle_logic.jl
@@ -76,9 +76,11 @@ function play_battle(starting_state::DynamicState, static_state::StaticState)
     state = starting_state
     while true
         weights1, weights2 = get_possible_decisions(state, static_state, 1), get_possible_decisions(state, static_state, 2)
-        (sum(weights1) * sum(weights2) == 0) && return get_battle_score(state, static_state)
-        decision1, decision2 = rand(Categorical(weights1 / sum(weights1))), rand(Categorical(weights2 / sum(weights2)))
-        state = play_turn(state, static_state, (decision1, decision2))
+        try
+            state = play_turn(state, static_state, (rand(Categorical(weights1 / sum(weights1))), rand(Categorical(weights2 / sum(weights2)))))
+        catch
+            return get_battle_score(state, static_state)
+        end
     end
 end
 
