e2e74ae0205b156a7eac6cbb285ec63a146ee59f
itscoe <itslagle@coe.edu>
2021-03-17 16:18:58 -0500

    typo in runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,7 +10,7 @@ using Test, BenchmarkTools
     @test isbits(static_state1)
     @test sizeof(dynamic_state1) == 34
     @test isbits(dynamic_state1)
-    res = @benchmark play_battle(dynamic_state, static_state)
+    res = @benchmark play_battle(dynamic_state1, static_state1)
     @test res.allocs == 0
     N = 10000
     @test sum(get_battle_scores(dynamic_state1, static_state1, N)) +
