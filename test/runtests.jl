9ece0b215b778486b24b9158db2eed59c7148a24
itscoe <itslagle@coe.edu>
2021-03-17 15:32:18 -0500

    Added tests for teams

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,17 @@ using RandomBattles
 using Test
 
 @testset "RandomBattles.jl" begin
-    # Write your own tests here.
+    static_state1 = StaticState(["lanturn" "dragonair" "froslass" "azumarill" "sableye" "marowak_alolan"])
+    dynamic_state1 = DynamicState(static_state1)
+    static_state2 = StaticState(["azumarill" "sableye" "marowak_alolan" "lanturn" "dragonair" "froslass"])
+    dynamic_state2 = DynamicState(static_state2)
+    @test sizeof(static_state1) == 168
+    @test isbits(static_state1)
+    @test sizeof(dynamic_state1) == 34
+    @test isbits(dynamic_state1)
+    res = @benchmark play_battle(dynamic_state, static_state)
+    @test res.allocs == 0
+    N = 10000
+    @test sum(get_battle_scores(dynamic_state1, static_state1, N)) +
+        sum(get_battle_scores(dynamic_state2, static_state2, N)) ≈ N atol=0.01*N
 end
