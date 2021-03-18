56a5b6d6ef34bba0181d4836463f5d2cc8000ce8
itscoe <itslagle@coe.edu>
2021-03-18 15:49:11 -0500

    Undid all of the changes with inbounds

--- src/team/battle_logic.jl
+++ src/team/battle_logic.jl
@@ -32,24 +32,24 @@ end
 function play_turn(state::DynamicState, static_state::StaticState, decision::Tuple{Int64,Int64})
     next_state = state
 
-    if @inbounds next_state.fastMovesPending[1] == Int8(0)
+    @inbounds if next_state.fastMovesPending[1] == Int8(0)
         next_state = evaluate_fast_moves(next_state, static_state, Int8(1))
     end
-    if @inbounds next_state.fastMovesPending[2] == Int8(0)
+    @inbounds if next_state.fastMovesPending[2] == Int8(0)
         next_state = evaluate_fast_moves(next_state, static_state, Int8(2))
     end
 
-    next_state = @inbounds step_timers(next_state,
+    @inbounds next_state = step_timers(next_state,
         2 < decision[1] < 5 ? static_state.teams[1].mons[next_state.teams[1].active].fastMove.cooldown : Int8(0),
         2 < decision[2] < 5 ? static_state.teams[2].mons[next_state.teams[2].active].fastMove.cooldown : Int8(0))
 
-    if @inbounds 8 < decision[1]
+    @inbounds if 8 < decision[1]
         next_state = evaluate_switch(next_state, Int8(1),
             decision[1] < 11 || 14 < decision[1] < 17 ? Int8(1) :
             decision[1] < 13 || 16 < decision[1] < 19 ? Int8(2) : Int8(3),
             decision[1] < 15 ? Int8(0) : Int8(24))
     end
-    if @inbounds 9 < decision[2]
+    @inbounds if 8 < decision[2]
         next_state = evaluate_switch(next_state, Int8(2),
             decision[2] < 11 || 14 < decision[2] < 17 ? Int8(1) :
             decision[2] < 13 || 16 < decision[2] < 19 ? Int8(2) : Int8(3),
@@ -57,19 +57,19 @@ function play_turn(state::DynamicState, static_state::StaticState, decision::Tup
     end
 
     cmp = get_cmp(next_state, static_state, 4 < decision[1] < 9, 4 < decision[2] < 9)
-    if @inbounds cmp[1] != Int8(0)
+    @inbounds if cmp[1] != Int8(0)
         @inbounds next_state = evaluate_charged_moves(next_state, static_state, cmp[1],
             decision[cmp[1]] < 7 ? Int8(1) : Int8(2), Int8(100), iseven(decision[get_other_agent(cmp[1])]),
             rand(Int8(0):Int8(99)) < static_state.teams[cmp[1]].mons[next_state.teams[cmp[1]].active].chargedMoves[decision[cmp[1]] < 7 ? Int8(1) : Int8(2)].buffChance)
-        if @inbounds next_state.fastMovesPending[get_other_agent(cmp[1])] != Int8(-1)
+        @inbounds if next_state.fastMovesPending[get_other_agent(cmp[1])] != Int8(-1)
             @inbounds next_state = evaluate_fast_moves(next_state, static_state, cmp[1])
         end
     end
-    if @inbounds cmp[2] != Int8(0)
+    @inbounds if cmp[2] != Int8(0)
         @inbounds next_state = evaluate_charged_moves(next_state, static_state, cmp[2],
             decision[cmp[2]] < 7 ? Int8(1) : Int8(2), Int8(100), iseven(decision[cmp[1]]),
             rand(Int8(0):Int8(99)) < static_state.teams[cmp[2]].mons[next_state.teams[cmp[2]].active].chargedMoves[decision[cmp[2]] < 7 ? Int8(1) : Int8(2)].buffChance)
-        if @inbounds next_state.fastMovesPending[cmp[1]] != Int8(-1)
+        @inbounds if next_state.fastMovesPending[cmp[1]] != Int8(-1)
             @inbounds next_state = evaluate_fast_moves(next_state, static_state, cmp[2])
         end
     end
