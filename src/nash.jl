581d7b1dc833aabd34d5bf074706c60201bccb16
itscoe <itslagle@coe.edu>
2021-03-27 00:12:15 -0500

    added alpha-beta pruning prototype

--- src/nash.jl
+++ src/nash.jl
@@ -11,6 +11,34 @@ maxmin(R::Matrix{Float64}, n::Int64) = @inbounds mapreduce(x -> minimum(R[x, :])
 findminmax(R::Matrix{Float64}, n::Int64) = @inbounds strat_vec(n, argmax(map(x -> minimum(R[x, :]), 1:n)))
 findmaxmin(R::Matrix{Float64}, m::Int64) = @inbounds strat_vec(m, argmin(map(x -> maximum(R[:, x]), 1:m)))
 
+function get_α(P::Matrix{Float64}, e, f)
+    # Set up model and payoff
+    model = direct_model(GLPK.Optimizer())
+
+    # Solve for row player
+    @variable(model, x[1:n], lower_bound = 0.0)
+    @constraint(model, sum(x) == 1.0)
+    @constraint(model, x' * P .>= f)
+
+    @objective(model, Max, x' * e)
+    optimize!(model)
+    return JuMP.objective_value(model)
+end
+
+function get_β(O::Matrix{Float64}, e, f)
+    # Set up model and payoff
+    model = direct_model(GLPK.Optimizer())
+
+    # Solve for row player
+    @variable(model, x[1:n], lower_bound = 0.0)
+    @constraint(model, sum(x) == 1.0)
+    @constraint(model, O * x' .<= f)
+
+    @objective(model, Min, e * x')
+    optimize!(model)
+    return JuMP.objective_value(model)
+end
+
 function nash(R::Matrix{Float64})
     n, m = size(R)
 
@@ -35,7 +63,7 @@ function nash(R::Matrix{Float64})
 end
 
 function SM(state::DynamicState, static_state::StaticState, depth::Int64; allow_waiting::Bool = false,
-  max_depth::Int64 = 15, sim_to_end::Bool = false)
+  sim_to_end::Bool = false)
     A, B = get_simultaneous_decisions(state, static_state, allow_waiting = allow_waiting)
     (length(A) == 0 || depth == 0) &&
         return sim_to_end ? (sum(get_battle_scores(state, static_state, 100) / 100) - 0.5,
@@ -52,11 +80,66 @@ function SM(state::DynamicState, static_state::StaticState, depth::Int64; allow_
             @inbounds payoffs[i, j] = payoffs[i, j - 1]
         else
             @inbounds payoffs[i, j] = SM(play_turn(state, static_state, (A[i], B[j])),
-                static_state, depth - 1, allow_waiting = allow_waiting, sim_to_end = sim_to_end,
-                max_depth = max_depth)[1]
+                static_state, depth - 1, allow_waiting = allow_waiting, sim_to_end = sim_to_end)[1]
         end
     end
     return nash(payoffs)
+end
+
+function SMAB(state::DynamicState, static_state::StaticState, α₀::Float64,
+  β₀::Float64, depth::Int64; allow_waiting::Bool = false, sim_to_end::Bool = false, ϵ = 0.001)
+    A, B = get_simultaneous_decisions(state, static_state, allow_waiting = allow_waiting)
+    (length(A) == 0 || depth == 0) &&
+        return sim_to_end ? (sum(get_battle_scores(state, static_state, 100) / 100) - 0.5,
+        vec([1.0]), vec([1.0])) : (get_battle_score(state, static_state) - 0.5,
+        vec([1.0]), vec([1.0]))
+
+    Q = [play_turn(state, static_state, (a, b)) for a in A, for b in B]
+    P = vcat(map(q -> get_min_score(q, static_state), Q), repeat(α₀, 1, length(B)))
+    O = hcat(map(q -> get_max_score(q, static_state), Q), repeat(β₀, length(A), 1))
+    non_dominated_rows = trues(length(A) + 1)
+    non_dominated_columns = trues(length(B))
+    non_dominated_rows[end] = false
+
+    for i in 1:length(A), j in 1:length(B)
+        if non_dominated_rows[i] && non_dominated_columns[j]
+            @inbounds if (5 in B || 7 in B) && !(5 <= B[j] <= 8) && iseven(A[i]) &&
+              (5 in A || 7 in A) && !(5 <= A[i] <= 8) && iseven(B[j])
+                @inbounds P[i, j] = P[i - 1, j - 1]
+                @inbounds O[i, j] = O[i - 1, j - 1]
+            elseif @inbounds (5 in B || 7 in B) && !(5 <= B[j] <= 8) && iseven(A[i])
+                @inbounds P[i, j] = P[i - 1, j]
+                @inbounds O[i, j] = O[i - 1, j]
+            elseif @inbounds (5 in A || 7 in A) && !(5 <= A[i] <= 8) && iseven(B[j])
+                @inbounds P[i, j] = P[i, j - 1]
+                @inbounds O[i, j] = O[i, j - 1]
+            else
+                α = get_α(P[1:end .!= i, 1:end .!= j], P[1:end .!= i, j], O[i, 1:end-1 .!= j])
+                β = get_β(O[1:end .!= i, 1:end .!= j], O[i, 1:end .!= j], P[1:end-1 .!= i, j])
+                if α < β
+                    @inbounds v = SMAB(Q[i, j], static_state, α, β, depth - 1,
+                        allow_waiting = allow_waiting, sim_to_end = sim_to_end)[1]
+                    if v <= α
+                        @inbounds dominated_rows[i] = false
+                    elseif v >= β
+                        @inbounds dominated_columns[j] = false
+                    else
+                        P[i, j] = v
+                        O[i, j] = v
+                    end
+                else
+                    @inbounds v = SMAB(Q[i, j], static_state, α, α + ϵ, depth - 1,
+                        allow_waiting = allow_waiting, sim_to_end = sim_to_end)[1]
+                    if v <= α
+                        @inbounds dominated_rows[i] = false
+                    else
+                        @inbounds dominated_columns[j] = false
+                    end
+                end
+            end
+        end
+    end
+    return nash(P[non_dominated_rows, non_dominated_columns])
 end
 
 function SM(state::DynamicIndividualState, static_state::StaticIndividualState, depth::Int64; allow_waiting::Bool = false,
