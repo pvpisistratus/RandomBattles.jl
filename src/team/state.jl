3e405b40371b18e0ed156a0f9d597ac603812d09
itscoe <itslagle@coe.edu>
2021-04-06 11:24:50 -0500

    Possible fix for state.jl

--- src/team/state.jl
+++ src/team/state.jl
@@ -12,9 +12,9 @@ end
 StaticState(teams::Array{String}; league = "great", cup = "open") =
     StaticState([
         StaticTeam(teams[1:3], league = league, cup = cup,
-            opponents = @SVector [i for i in StaticPokemon.(teams[4:6], league = league, cup = cup)]),
+            opponents = @SVector [i for i in StaticPokemon.($teams[4:6], league = league, cup = cup)]),
         StaticTeam(teams[4:6], league = league, cup = cup,
-            opponents = @SVector [i for i in StaticPokemon.(teams[1:3], league = league, cup = cup)])
+            opponents = @SVector [i for i in StaticPokemon.($teams[1:3], league = league, cup = cup)])
     ])
 
 StaticState(team1::StaticTeam, team2::StaticTeam) = StaticState([team1, team2])
