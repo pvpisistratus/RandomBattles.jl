961a82d7ee4d5ffb185259d6f25e13422b6eada6
itscoe <itslagle@coe.edu>
2021-04-08 15:17:42 -0500

    nash always outputs NashResult

--- src/nash.jl
+++ src/nash.jl
@@ -41,7 +41,7 @@ function get_β(O::Matrix{Float64}, e::Vector{Float64}, f::Vector{Float64})
     return JuMP.objective_value(model)
 end
 
-struct NashResults
+struct NashResult
     z::Float64
     x::Vector{Float64}
     y::Vector{Float64}
@@ -51,9 +51,9 @@ function nash(R::Matrix{Float64})
     n, m = size(R)
 
     # Check if we have to do linear programming
-    n == 1 && return minimum(R), vec([1.0]), strat_vec(m, argmin(vec(R)))
-    m == 1 && return maximum(R), strat_vec(n, argmin(vec(R))), vec([1.0])
-    minmax(R, m) == maxmin(R, n) && return minmax(R, m), findminmax(R, n), findmaxmin(R, m)
+    n == 1 && return NashResult(minimum(R), vec([1.0]), strat_vec(m, argmin(vec(R))))
+    m == 1 && return NashResult(maximum(R), strat_vec(n, argmin(vec(R))), vec([1.0]))
+    minmax(R, m) == maxmin(R, n) && return NashResult(minmax(R, m), findminmax(R, n), findmaxmin(R, m))
 
     # Set up model and payoff
     model = direct_model(GLPK.Optimizer())
@@ -67,15 +67,15 @@ function nash(R::Matrix{Float64})
 
     optimize!(model)
 
-    return NashResults(JuMP.value(z), JuMP.value.(x), shadow_price.(c1))
+    return NashResult(JuMP.value(z), JuMP.value.(x), shadow_price.(c1))
 end
 
 function SM(state::DynamicState, static_state::StaticState, depth::Int64; allow_waiting::Bool = false,
   sim_to_end::Bool = false)
     A, B = get_simultaneous_decisions(state, static_state, allow_waiting = allow_waiting)
     (length(A) == 0 || depth == 0) &&
-        return sim_to_end ? NashResults(sum(get_battle_scores(state, static_state, 100) / 100) - 0.5,
-        vec([1.0]), vec([1.0])) : NashResults(get_battle_score(state, static_state) - 0.5,
+        return sim_to_end ? NashResult(sum(get_battle_scores(state, static_state, 100) / 100) - 0.5,
+        vec([1.0]), vec([1.0])) : NashResult(get_battle_score(state, static_state) - 0.5,
         vec([1.0]), vec([1.0]))
     payoffs = zeros(Float64, length(A), length(B))
     for i in 1:length(A), j in 1:length(B)
@@ -98,8 +98,8 @@ function SMAB(state::DynamicState, static_state::StaticState, α₀::Float64,
   β₀::Float64, depth::Int64; allow_waiting::Bool = false, sim_to_end::Bool = false, ϵ = 0.001)
     A, B = get_simultaneous_decisions(state, static_state, allow_waiting = allow_waiting)
     (length(A) == 0 || depth == 0) &&
-        return sim_to_end ? NashResults(sum(get_battle_scores(state, static_state, 100) / 100) - 0.5,
-        vec([1.0]), vec([1.0])) : NashResults(get_battle_score(state, static_state) - 0.5,
+        return sim_to_end ? NashResult(sum(get_battle_scores(state, static_state, 100) / 100) - 0.5,
+        vec([1.0]), vec([1.0])) : NashResult(get_battle_score(state, static_state) - 0.5,
         vec([1.0]), vec([1.0]))
 
     Q = [play_turn(state, static_state, (a, b)) for a in A, b in B]
@@ -153,8 +153,8 @@ function SM(state::DynamicIndividualState, static_state::StaticIndividualState, 
     allow_overfarming::Bool = false, max_depth = 15::Int64, sim_to_end::Bool = false)
     A, B = get_simultaneous_decisions(state, static_state, allow_waiting = allow_waiting, allow_overfarming = allow_overfarming)
     (length(A) == 0 || depth == 0) &&
-        return sim_to_end ? NashResults(sum(get_battle_scores(state, static_state, 100) / 100) - 0.5,
-        vec([1.0]), vec([1.0])) : NashResults(get_battle_score(state, static_state) - 0.5,
+        return sim_to_end ? NashResult(sum(get_battle_scores(state, static_state, 100) / 100) - 0.5,
+        vec([1.0]), vec([1.0])) : NashResult(get_battle_score(state, static_state) - 0.5,
         vec([1.0]), vec([1.0]))
     payoffs = zeros(Float64, length(A), length(B))
     for i in 1:length(A), j in 1:length(B)
