819fc463f0bb21a0b47162c25855ed4dd2b55c9e
itscoe <itslagle@coe.edu>
2021-07-17 02:24:23 -0500

    used shortened variable names in battle_score

--- src/team/mechanics.jl
+++ src/team/mechanics.jl
@@ -327,5 +327,4 @@ the PvPoke-like score for the battle. Note that this can also be computed for
 battles in progress, and thus differs from PvPoke's use cases
 """
 battle_score(s::DynamicState, static_s::StaticState) =
-    get_min_score(state, static_state) +
-        get_max_score(state, static_state) - 0.5
+    get_min_score(s, static_s) + get_max_score(s, static_s) - 0.5
